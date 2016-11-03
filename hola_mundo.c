258ab599e6eae6e9ce34869a71be0246a68b429d
Ferch2109 <marifer.gusanito@gmail.com>
2016-11-02 19:48:36 -0600

    Hola mundo! :'3

---
+++ hola_mundo.c
@@ -0,0 +1,12 @@
+//Librería de operaciones estandar, entrada-salida,tipos
+#include<stdio.h>
+
+//Fumción principal del programa, devuelve un int y recive void (accion)
+int main( void ){
+
+	//mediante la función printf imprimimos en consola la frase '¡Hola, mundo!'
+	printf( "¡Hola, mundo!" );
+	//regresamos 0
+	return 0;
+
+}
